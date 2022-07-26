e378faf617025284586fb68c7da7b31d5428fa1f
Barillas <jbarillas-2017174@kinal.edu.gt>
2022-07-26 14:07:41 -0600

    Vistas users, company, profile

--- VycenFront/src/app/app-routing.module.ts
+++ VycenFront/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { PreloadingStrategy, RouterModule, Routes } from '@angular/router';
+import { CompanyComponent } from './components/company/company.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { RegisterComponent } from './components/register/register.component';
+import { UsersComponent } from './components/users/users.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -12,6 +14,8 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'profile', component: ProfileComponent },
+  { path: 'company', component: CompanyComponent },
+  { path: 'users', component: UsersComponent },
   { path: '**', component: NotFoundComponent }
 ];
 
--- VycenFront/src/app/components/profile/profile.component.ts
+++ VycenFront/src/app/components/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-profile',
@@ -7,8 +8,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
+  swalWithBootstrapButtons = Swal.mixin({
+    customClass: {
+      confirmButton: 'btn btn-success',
+      cancelButton: 'btn btn-danger'
+    },
+    buttonsStyling: true
+  })
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+
+  delete(){
+    console.log('hola')
+  }
+  deleteAccount() {
+    Swal.fire({
+      title: 'Are you sure to delete your account?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Yes, delete it!',
+      cancelButtonText: 'Cancel!',
+      reverseButtons: true
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire(
+          'Deleted!',
+          'Your account has been deleted.',
+          'success'
+        )
+        this.delete()
+      } else if (
+        /* Read more about handling dismissals below */
+        result.dismiss === Swal.DismissReason.cancel
+      ) {
+        Swal.fire({
+          title: 'Canceled',
+          text: 'Your account is safe',
+          icon: 'error',
+          showConfirmButton: false,
+          timer: 2000
+        })
+      }
+    })
   }
 }
