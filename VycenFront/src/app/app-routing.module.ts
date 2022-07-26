7517fc00d992f4dc15ca8e7478edc2f3662205ad
Barillas <jbarillas-2017174@kinal.edu.gt>
2022-07-26 11:04:54 -0600

    Profile, footer, navbar componente

--- VycenFront/src/app/app-routing.module.ts
+++ VycenFront/src/app/app-routing.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { PreloadingStrategy, RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { ProfileComponent } from './components/profile/profile.component';
 import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent }
+  { path: 'register', component: RegisterComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: '**', component: NotFoundComponent }
 ];
 
 @NgModule({
