7e16852b7f882cc10d372842ff00a7081d6a2e06
Barillas <jbarillas-2017174@kinal.edu.gt>
2022-07-28 18:01:58 -0600

    foro actualizado

--- server/src/routes/forum.routes.js
+++ server/src/routes/forum.routes.js
@@ -6,9 +6,9 @@ const connectMultiparty = require('connect-multiparty');
 const upload = connectMultiparty({ uploadDir: './uploads/img' });
 const api = express.Router();
 const mdAuth = require('../services/authenticated');
-
+/*
 api.post('/uploadImg', [mdAuth.ensureAuth, upload], forumController.addImage);
 api.get('/getImage/:fileName', upload, forumController.getImage);
 api.get('/getImages', upload, forumController.getImages);
-
+*/
 module.exports = api;
