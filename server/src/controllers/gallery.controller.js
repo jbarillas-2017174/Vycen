4803a9429f5fb14e84d4626de0c239f7c9719f75
Barillas <jbarillas-2017174@kinal.edu.gt>
2023-11-20 13:55:04 -0600

    Implementación de obntener imagenes

--- server/src/controllers/gallery.controller.js
+++ server/src/controllers/gallery.controller.js
@@ -1,6 +1,7 @@
 'use strict'
 const firebase = require('firebase/storage');
 const fs = require('fs');
+const path = require('path')
 
 exports.prueba = async (req, res) => {
     console.log('PRUEBA')
@@ -11,8 +12,8 @@ exports.uploadImage = async (req, res) => {
     try {
         const imageBlob = req.file.buffer;
         const imageName = req.file.originalname;
-        fs.writeFileSync('./src/assets/'+imageName, Buffer.from(imageBlob))
-        return res.send({message: imageName})
+        fs.writeFileSync('./src/assets/' + imageName, Buffer.from(imageBlob))
+        return res.send({ message: imageName })
         // const storage = firebase.getStorage();
         // const storageRef = firebase.ref(storage, imageName);
         // let responseFirebase = await firebase.uploadBytes(storageRef, imageBlob);
@@ -28,16 +29,39 @@ exports.uploadImage = async (req, res) => {
 }
 
 exports.getImages = async (req, res) => {
-    const storage = firebase.getStorage();
-    const storageRef = firebase.ref(storage);
-    let fileNames = []
-    let files = await firebase.listAll(storageRef)
+    const directorio = './src/assets';
+    fs.readdir(directorio, (error, archivos) => {
+        if (error) {
+            console.error('Error al leer el directorio:', error);
+            return res.status(500).send({ message: 'Error al leer el directorio' });
+        }
 
-    await Promise.all(files.items.map(async (itemRef) => {
-        const fileName = itemRef.name;
-        const fileDownloadURL = await firebase.getDownloadURL(itemRef);
-        fileNames.push({ name: fileName, downloadURL: fileDownloadURL });
-      }));
+        // Filtrar solo los archivos con extensión de imagen (por ejemplo, .png)
+        const imagenes = archivos.filter(archivo =>
+            ['.png', '.jpg', '.jpeg', '.gif'].includes(path.extname(archivo).toLowerCase())
+        );
 
-    return res.send({ files: fileNames });
+        // Mapear los nombres de archivo a las rutas completas
+        const rutasImagenes = imagenes.map(archivo => `${directorio}/${archivo}`);
+
+        // Leer cada imagen y enviarlas como respuesta
+        const imagenesEnBytes = rutasImagenes.map(ruta => ({
+            nombre: path.basename(ruta), // Obtener el nombre del archivo
+            imagen: fs.readFileSync(ruta, 'base64') // Leer la imagen en base64
+        }));
+
+        res.send({ imagenes: imagenesEnBytes });
+    });
+    // const storage = firebase.getStorage();
+    // const storageRef = firebase.ref(storage);
+    // let fileNames = []
+    // let files = await firebase.listAll(storageRef)
+
+    // await Promise.all(files.items.map(async (itemRef) => {
+    //     const fileName = itemRef.name;
+    //     const fileDownloadURL = await firebase.getDownloadURL(itemRef);
+    //     fileNames.push({ name: fileName, downloadURL: fileDownloadURL });
+    // }));
+
+    // return res.send({ files: fileNames });
 }
